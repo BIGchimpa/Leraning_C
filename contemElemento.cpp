eaf8eec897f5d7e1153706e1da5e7b44885d0a23
BIGchimpa <eduardo.gofenet@unesp.br>
2025-05-27 10:55:20 -0300

    Create contemElemento.cpp

---
+++ contemElemento.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+
+bool estaOuNaoEsta(A[],int T, int x){
+    for (i = 0; i < T, i++){
+        if(A[i] == x){
+            return true;
+        }
+    }
+    return false;
+}
