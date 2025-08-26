35b76ab3421a8a191cf154184a9480ca1a5d6428
BIGchimpa <eduardo.gofenet@unesp.br>
2025-08-26 10:05:40 -0300

    INICIAIS

---
+++ iniciais.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    puts("EEEEEEEEEEEE\nE    E     E\nE    E     E\nE    E     E\nE          E");
+    puts("\nDDDDDDDDDDDD\nD          D\nD          D\n D        D\n  DDDDDDDD");
+    puts("\n  UUUUUUUUUU\n U\nU\nU\n U\n  UUUUUUUUUU");
+
+    return 0;
+}
